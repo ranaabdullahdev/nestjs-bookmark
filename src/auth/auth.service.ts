ea2719e48b74615caefb9b07bd785967669c021f
Muhammad Abdullah <rana.abdullah3655@gmail.com>
2024-09-18 15:33:22 +0500

    First migaration run successfully

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { User,Bookmark } from '@prisma/client';
+
+@Injectable({})
+export class AuthService {
+  signin() {
+    return { msg: 'I have signed in' };
+  }
+  signup() {
+    return { msg: 'I have signed up' };
+  }
+}
