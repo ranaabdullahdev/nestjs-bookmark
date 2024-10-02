7e02d2e5be006693e61130a63836ee15b6d655c2
Muhammad Abdullah <rana.abdullah3655@gmail.com>
2024-10-02 20:00:40 +0500

    Jwt functionality implemented

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,48 +1,101 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 
 import * as bcrypt from 'bcrypt';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
-
-  async signin(dto: AuthDto) {
-    const hash = await bcrypt.hash(
-      dto.password,
-      10,
-    );
-
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
-    return user;
-  }
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signup(dto: AuthDto) {
-    const hash = await bcrypt.hash(
+    try {
+      const hash = await bcrypt.hash(
+        dto.password,
+        10,
+      );
+
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+
+      return this.signToken(user.id, user.email);
+    } catch (error) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'Email already exists',
+          );
+        }
+      }
+      throw error;
+    }
+  }
+
+  async signin(dto: AuthDto) {
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+
+    if (!user) {
+      throw new ForbiddenException(
+        'User not found',
+      );
+    }
+
+    const isValidPass = await bcrypt.compare(
       dto.password,
-      10,
+      user.hash,
     );
 
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-      select:{
-        id:true,
-        email:true,
-        createdAt:true,
-      }
-    });
-    
+    if (!isValidPass) {
+      throw new ForbiddenException(
+        'Invalid email or password',
+      );
+    }
 
-    return user;
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = { sub: userId, email };
+
+    const secret = this.config.get('JWT_SECRET');
+    const token = await this.jwt.sign(payload, {
+      secret: secret,
+      expiresIn: '15m',
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import {
+  ExtractJwt,
+  Strategy,
+} from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(
+  Strategy,
+) {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest:
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+  async validate(payload) {
+    return {
+      id: payload.sub,
+      email: payload.email,
+    };
+  }
+  F;
+}
