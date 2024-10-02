2ffe0fe0d92c6f9c7f94c55c2df8ecb5e8e93639
Muhammad Abdullah <rana.abdullah3655@gmail.com>
2024-10-02 18:03:08 +0500

    prisma models got updated

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,48 @@
 import { Injectable } from '@nestjs/common';
-import { User,Bookmark } from '@prisma/client';
+import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
-@Injectable({})
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
 export class AuthService {
-  signin() {
-    return { msg: 'I have signed in' };
+  constructor(private prisma: PrismaService) {}
+
+  async signin(dto: AuthDto) {
+    const hash = await bcrypt.hash(
+      dto.password,
+      10,
+    );
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+    return user;
   }
-  signup() {
-    return { msg: 'I have signed up' };
+
+  async signup(dto: AuthDto) {
+    const hash = await bcrypt.hash(
+      dto.password,
+      10,
+    );
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+      select:{
+        id:true,
+        email:true,
+        createdAt:true,
+      }
+    });
+    
+
+    return user;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
-  console.log(' nestjs app is running on port 8000');
+  app.useGlobalPipes(new ValidationPipe( {whitelist: true}));
+  await app.listen(8001);
+  console.log(' nestjs app is running on port 8001');
 }
 bootstrap();
